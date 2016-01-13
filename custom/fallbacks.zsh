8072ff7e3de89a563692444fdee563d9edf47b6a
Max Falk <gmdfalk@gmail.com>
2016-01-13 01:03:55 +0100

    fix clear fallback alias

--- custom/fallbacks.zsh
+++ custom/fallbacks.zsh
@@ -1,5 +1,5 @@
-if ! command -v asdf &>/dev/null; then
-    alias cl='printf "\033c"'
+if ! command -v clear &>/dev/null; then
+    alias clear='printf "\033c"'
 fi
 if command -v hub &>/dev/null; then
     alias git=hub
